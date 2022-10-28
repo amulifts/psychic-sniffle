6bfd5bdb7f40e5110ec86218b33c57b30f3fc84d
Aman Khadka <amulifts@gmail.com>
2022-10-29 05:01:22 +0545

    psychicbot

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const moment = require("moment");
 const simpleGit = require("simple-git");
 const random = require("random");
 
-
 const FILE_PATH = "./data.json";
-
 
 const makeCommit = n => {
     if (n === 0) return simpleGit().push();
@@ -23,6 +21,5 @@ const makeCommit = n => {
 
     simpleGit().add([FILE_PATH]).commit(DATE, {"--date": DATE}, makeCommit.bind(this, --n));
 };
-
 
 makeCommit(500);
